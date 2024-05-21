68062f21307c05ba77885ce90ad58fa0d9ff6444
Pradipkakde <kakde.pradip1@gmail.com>
2024-05-21 09:58:10 +0530

    Added Api Integration

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../Services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-seller-add-product',
@@ -6,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seller-add-product.component.css']
 })
 export class SellerAddProductComponent implements OnInit{
+  addProductMessage:string | undefined;
+  constructor(private product:ProductService){}
   ngOnInit(): void {
     
   }
-  submit(data:object){
-    console.warn(data)
+  submit(data:product){
+    this.product.addProudct(data).subscribe((result)=>{
+      console.warn(result)
+      if(result){
+        this.addProductMessage="Product Successfully added";
+      }
+      setTimeout(() =>this.addProductMessage=undefined,3000 );
+        
+      
+    })
 
   }
 
