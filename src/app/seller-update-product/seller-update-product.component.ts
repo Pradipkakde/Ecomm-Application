d014b96a2bb398f1596954b3574e0cc7f383b690
Pradipkakde <kakde.pradip1@gmail.com>
2024-05-23 21:22:16 +0530

    Populate (prefilled) form in update component

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../Services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-seller-update-product',
@@ -6,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seller-update-product.component.css']
 })
 export class SellerUpdateProductComponent implements OnInit{
+  productData:undefined |product
+
+  constructor(private route:ActivatedRoute ,private product:ProductService){}
+
   ngOnInit(): void {
+
+    let productId= this.route.snapshot.paramMap.get('id');
+    console.log(productId)
     
+   productId &&this.product.getproduct(productId).subscribe((data)=>{
+    console.log(data)
+    this.productData=data;
+   })
+
   }
+
   submit(data:any){
     console.warn("Updated")
   }
